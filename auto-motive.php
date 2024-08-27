46bfa49d3dacb8736f0acd4bba3778a23c48706f
prishpon <prishpon@gmail.com>
2024-08-27 11:58:42 +0200

    inserting data from database to CPT

--- auto-motive.php
+++ auto-motive.php
@@ -14,7 +14,7 @@
  * - filter the table to compare with saved meta id
  
 */
-// CPT
+
 function create_auto_cpt() {
 
     $labels = array(
@@ -25,14 +25,26 @@ function create_auto_cpt() {
         'archives' => __( 'Archivi Auto', 'Auto' ),
         'attributes' => __( 'Attributi delle Auto', 'Auto' ),
         'parent_item_colon' => __( 'Genitori Auto:', 'Auto' ),
-        'all_items' => __( 'All Autos', 'Auto' ),
-        'add_new_item' => __( 'Add new Auto', 'Auto' ),
-        'add_new' => __( 'New', 'Auto' ),
+        'all_items' => __( 'Tutti le Auto', 'Auto' ),
+        'add_new_item' => __( 'Aggiungi nuova Auto', 'Auto' ),
+        'add_new' => __( 'Nuovo', 'Auto' ),
         'new_item' => __( 'Auto redigere', 'Auto' ),
-        'edit_item' => __( 'Edit Auto', 'Auto' ),
-        'update_item' => __( 'Update Auto', 'Auto' ),
-        'view_item' => __( 'View Auto', 'Auto' ),
-        'view_items' => __( 'View all', 'Auto' )
+        'edit_item' => __( 'Modifica Auto', 'Auto' ),
+        'update_item' => __( 'Aggiorna Auto', 'Auto' ),
+        'view_item' => __( 'Visualizza Auto', 'Auto' ),
+        'view_items' => __( 'Visualizza le Auto', 'Auto' ),
+        'search_items' => __( 'Cerca Auto', 'Auto' ),
+        'not_found' => __( 'Nessun Auto trovato.', 'Auto' ),
+        'not_found_in_trash' => __( 'Nessun Auto trovato nel cestino.', 'Auto' ),
+        'featured_image' => __( 'Immagine in evidenza', 'Auto' ),
+        'set_featured_image' => __( 'Imposta immagine in evidenza', 'Auto' ),
+        'remove_featured_image' => __( 'Rimuovi immagine in evidenza', 'Auto' ),
+        'use_featured_image' => __( 'Usa come immagine in evidenza', 'Auto' ),
+        'insert_into_item' => __( 'Inserisci nelle Auto', 'Auto' ),
+        'uploaded_to_this_item' => __( 'Caricato in questo Auto', 'Auto' ),
+        'items_list' => __( 'Elenco degli Auto', 'Auto' ),
+        'items_list_navigation' => __( 'Navigazione elenco Auto', 'Auto' ),
+        'filter_items_list' => __( 'Filtra elenco Auto', 'Auto' ),
     );
     $args = array(
         'label' => __( 'Auto', 'Auto' ),
@@ -60,7 +72,6 @@ function create_auto_cpt() {
 }
 add_action( 'init', 'create_auto_cpt', 0 );
 
-//add metaboxes
 add_action( 'admin_init', 'my_admin' );
 
 function my_admin() {
@@ -78,15 +89,15 @@ function display_car_review_meta_box() {
     ?>
     <table>
         <tr>
-            <td style="width: 50%">ID</td>
+            <td style="width: 50%">UIID</td>
             <td><input type="text" size="40" name="garage" value="<?php echo get_post_meta( get_the_ID(), 'id', true ); ?>" readonly /></td>
         </tr>
         <tr>
-            <td style="width: 50%">Brand</td>
+            <td style="width: 50%">Marca</td>
             <td><input type="text" size="40" name="garage" value="<?php echo get_post_meta( get_the_ID(), 'brand', true ); ?>" readonly /></td>
         </tr>
         <tr>
-            <td style="width: 50%">Model</td>
+            <td style="width: 50%">Modello</td>
             <td><input type="text" size="40" name="garage" value="<?php echo get_post_meta( get_the_ID(), 'model', true ); ?>" readonly /></td>       
         </tr>
         <tr>
@@ -94,43 +105,69 @@ function display_car_review_meta_box() {
             <td><input type="text" size="40" name="garage" value="<?php echo get_post_meta( get_the_ID(), 'color', true ); ?>" readonly /></td>
         </tr>
         <tr>
-            <td style="width: 50%">KM</td>
+            <td style="width: 50%">Mileage</td>
             <td><input type="text" size="40" name="garage" value="<?php echo get_post_meta( get_the_ID(), 'km', true ); ?>" readonly /></td>       
         </tr>
     </table>
     <?php
 }
 
-//query post CPT cars to check is their similar id in database
-function am_check_for_similar_meta_id(){
+add_action( 'wp', 'automotive_insert_into_auto_cpt' );
 
-    $id_array_in_cpt = [];
+function automotive_check_for_similar_meta_ids() {
+    $id_arrays_in_cpt = array();
 
-    $args = [
+    $args = array(
+        'post_type'      => 'auto',
         'posts_per_page' => -1,
-        'post_type'      => 'auto'
-    ];
+    );
 
-    $query = new Query($args);
-
-    while($query->have_posts()){
-        $query->the_post();
-        $id_array_in_cpt[] = get_post_meta(get_the_ID(),'id',true);
+    $loop = new WP_Query($args);
+    while( $loop->have_posts() ) {
+        $loop->the_post();
+        $id_arrays_in_cpt[] = get_post_meta( get_the_ID(), 'id', true );
     }
 
-
-return $id_array_in_cpt;
-
+    return $id_arrays_in_cpt;
 }
 
-function am_query_garage_table(){
+function automotive_query_garage_table( $car_available_in_cpt_array ) {
     global $wpdb;
-    $table_name =  $wpdb->prefix . 'garage';
-    $results = $wpdb->get_results("SELECT * FROM $table_name");
+    $table_name = $wpdb->prefix . 'garage';
 
-
-    return $results;
+    if ( NULL === $car_available_in_cpt_array || 0 === $car_available_in_cpt_array || '0' === $car_available_in_cpt_array || empty( $car_available_in_cpt_array ) ) {
+        $results = $wpdb->get_results("SELECT * FROM $table_name");
+        return $results;
+    } else {
+        $ids = implode( ",", $car_available_in_cpt_array );
+        $sql = "SELECT * FROM $table_name WHERE id NOT IN ( $ids )";
+        $results = $wpdb->get_results( $sql );
+        return $results;
+    }
 }
 
-//var dump results
-// add_action('wp','am_query_garage_table');
+function automotive_insert_into_auto_cpt() {
+
+    $car_available_in_cpt_array = automotive_check_for_similar_meta_ids();
+    $database_results = automotive_query_garage_table( $car_available_in_cpt_array );
+
+    if ( NULL === $database_results || 0 === $database_results || '0' === $database_results || empty( $database_results ) ) {
+        return;
+    }
+
+    foreach ( $database_results as $result ) {
+        $car_model = array(
+            'post_title' => wp_strip_all_tags( $result->Brand . ' ' . $result->Model . ' ' . $result->Km ),
+            'meta_input' => array(
+                'id'        => $result->id,
+                'brand'        => $result->Brand,
+                'model'        => $result->Model,
+                'color'        => $result->Color,
+                'km'           => $result->Km,
+            ),
+            'post_type'   => 'auto',
+            'post_status' => 'publish',
+        );
+        wp_insert_post( $car_model );
+    }
+}
